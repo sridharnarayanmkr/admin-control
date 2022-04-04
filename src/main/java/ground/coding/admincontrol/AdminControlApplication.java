f1f23a63eef765166aa8ffd48d319ad9937773ce
sridharnarayanmkr <sridharnarayanmkr@gmail.com>
2022-04-04 21:23:47 +0530

    1. Initial commit

---
+++ src/main/java/ground/coding/admincontrol/AdminControlApplication.java
@@ -0,0 +1,13 @@
+package ground.coding.admincontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ground/coding/admincontrol/AdminControlApplicationTests.java
@@ -0,0 +1,13 @@
+package ground.coding.admincontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
